76acd82999fb3a3a8f8627f4eb67962ffeb9f030
jonathan sol <simmynose9061@gmail.com>
2022-11-09 15:08:24 -0500

    Changes in Island Generator

--- IG/Assets/IslandGenerator.cs
+++ IG/Assets/IslandGenerator.cs
@@ -13,14 +13,21 @@ public class IslandGenerator : MonoBehaviour
     public Vector2 MountainVals;
 
     [Header("Island Modifiers")]
-    public Vector2 MinMaxRot;
-    public Vector2 MinMaxSize;
-    public Vector2 MinMaxSizeVariation;
-    public Vector2 MaxPosition;
+    public Vector2 IMinMaxRot;
+    public Vector2 IMinMaxSize;
+    public Vector2 IMinMaxSizeVariation;
+    public Vector2 IMaxPosition;
+
+    [Header("Mountain Modifiers")]
+    public Vector2 MMinMaxRot;
+    public Vector2 MMinMaxSize;
+    public Vector2 MMinMaxSizeVariation;
+    public Vector2 MMaxPosition;
     // Start is called before the first frame update
     void Start()
     {
         PlaceIslands();
+        PlaceMountains();
     }
 
     // Update is called once per frame
@@ -33,11 +40,24 @@ public class IslandGenerator : MonoBehaviour
     {
         for (int i = 0; i < Random.Range(IslandVals.x, IslandVals.y); i++)
         {
-            Quaternion IslandRotation = transform.rotation * Quaternion.Euler(0, Random.Range(MinMaxRot.x, MinMaxRot.y), 0);
-            Vector3 IslandPosition = new Vector3(Random.Range(-MaxPosition.x, MaxPosition.x), 0, Random.Range(-MaxPosition.y, MaxPosition.y));
-            float IslandBaseSize = Random.Range(MinMaxSize.x, MinMaxSize.y);
-            Vector3 IslandDimensions = new Vector3(IslandBaseSize + Random.Range(MinMaxSizeVariation.x, MinMaxSizeVariation.y), IslandBaseSize + Random.Range(MinMaxSizeVariation.x, MinMaxSizeVariation.y), IslandBaseSize + Random.Range(MinMaxSizeVariation.x, MinMaxSizeVariation.y));
+            Quaternion IslandRotation = transform.rotation * Quaternion.Euler(0, Random.Range(IMinMaxRot.x, IMinMaxRot.y), 0);
+            Vector3 IslandPosition = new Vector3(Random.Range(-IMaxPosition.x, IMaxPosition.x), 0, Random.Range(-IMaxPosition.y, IMaxPosition.y));
+            float IslandBaseSize = Random.Range(IMinMaxSize.x, IMinMaxSize.y);
+            Vector3 IslandDimensions = new Vector3(IslandBaseSize + Random.Range(IMinMaxSizeVariation.x, IMinMaxSizeVariation.y), IslandBaseSize + Random.Range(IMinMaxSizeVariation.x, IMinMaxSizeVariation.y), IslandBaseSize + Random.Range(IMinMaxSizeVariation.x, IMinMaxSizeVariation.y));
             GameObject Island = Instantiate(Islands[Random.Range(0, Islands.Length)], IslandPosition, IslandRotation);
+            Island.transform.localScale = IslandDimensions;
+        }
+    }
+
+    void PlaceMountains()
+    {
+        for (int i = 0; i < Random.Range(MountainVals.x, MountainVals.y); i++)
+        {
+            Quaternion IslandRotation = transform.rotation * Quaternion.Euler(0, Random.Range(MMinMaxRot.x, MMinMaxRot.y), 0);
+            Vector3 IslandPosition = new Vector3(Random.Range(-MMaxPosition.x, MMaxPosition.x), 0, Random.Range(-MMaxPosition.y, MMaxPosition.y));
+            float IslandBaseSize = Random.Range(MMinMaxSize.x, MMinMaxSize.y);
+            Vector3 IslandDimensions = new Vector3(IslandBaseSize + Random.Range(MMinMaxSizeVariation.x, MMinMaxSizeVariation.y), IslandBaseSize + Random.Range(MMinMaxSizeVariation.x, MMinMaxSizeVariation.y), IslandBaseSize + Random.Range(MMinMaxSizeVariation.x, MMinMaxSizeVariation.y));
+            GameObject Island = Instantiate(Mountains[Random.Range(0, Mountains.Length)], IslandPosition, IslandRotation);
             Island.transform.localScale = IslandDimensions;
         }
     }
